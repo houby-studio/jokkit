bff65b97074576dedf27e64302b44e4dd59daacd
Jakub Šindelář <megastary@proton.me>
2023-04-23 22:37:39 +0200

    Remove some stupid config file

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,7 +2,10 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, world! This is Jokkit." }));
+    let app = Router::new().route(
+        "/",
+        get(|| async { "Hello, world! This is Jokkit. This time for sure!" }),
+    );
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
