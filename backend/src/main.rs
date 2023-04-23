70a670ebdde38f24a947f124e675a9fc174d6bbb
Jakub Šindelář <megastary@proton.me>
2023-04-23 22:56:21 +0200

    uggh

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,7 +4,9 @@ use axum::{routing::get, Router};
 async fn main() {
     let app = Router::new().route(
         "/",
-        get(|| async { "Hello, world! This is Jokkit. This time for sure! We will delete this!" }),
+        get(|| async {
+            "Hello, world! This is Jokkit. This time for sure! We will delete this! Getting old.."
+        }),
     );
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
