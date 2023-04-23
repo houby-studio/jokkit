b186f387af41f6cd15a17f02f33f823a6607f78a
Jakub Šindelář <megastary@proton.me>
2023-04-23 22:32:23 +0200

    Customize default route

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,7 +2,7 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+    let app = Router::new().route("/", get(|| async { "Hello, world! This is Jokkit." }));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
